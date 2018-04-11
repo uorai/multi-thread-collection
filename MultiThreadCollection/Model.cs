129120ff07bfdfa0a4125911686f4c2924cc531b
uorai <ftorpedo7730@gmail.com>
2018-04-12 01:01:45 +0900

    sample code

---
+++ MultiThreadCollection/Model.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace MultiThreadCollection
+{
+    public class Model
+    {
+        private object _lockObject = new object();
+
+        public ObservableCollection<string> List { get; set; } = new ObservableCollection<string>();
+
+        public Model()
+        {
+            BindingOperations.EnableCollectionSynchronization(List, _lockObject);
+        }
+
+        public async Task Add()
+        {
+            await Task.Run(() =>
+            {
+                for (var i = 0; i < 100; i++)
+                {
+                    List.Add("hoge" + i.ToString());
+                }
+            });
+        }
+    }
+}
---
+++ MultiThreadCollection/ViewModel.cs
@@ -0,0 +1,32 @@
+﻿using Reactive.Bindings;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiThreadCollection
+{
+    public class ViewModel
+    {
+        private Model _model = new Model();
+
+        public ObservableCollection<string> List
+        {
+            get { return _model.List; }
+            set { _model.List = value; }
+        }
+
+        public AsyncReactiveCommand Command { get; }
+
+        public ViewModel()
+        {
+            Command = new AsyncReactiveCommand();
+            Command.Subscribe(async _ =>
+            {
+                await _model.Add();
+            });
+        }
+    }
+}
