8aa38a0cb6750bd130b43d4c369b46251a8dbfd3
uorai <ftorpedo7730@gmail.com>
2018-04-16 00:00:36 +0900

    wrote lock statement

--- MultiThreadCollection/Model.cs
+++ MultiThreadCollection/Model.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Data;
 
@@ -10,23 +11,47 @@ namespace MultiThreadCollection
 {
     public class Model
     {
+        private static Model _model = new Model();
         private object _lockObject = new object();
 
-        public ObservableCollection<string> List { get; set; } = new ObservableCollection<string>();
+        public ObservableCollection<DataGridSource> List { get; set; } = new ObservableCollection<DataGridSource>();
 
-        public Model()
+        private Model()
         {
             BindingOperations.EnableCollectionSynchronization(List, _lockObject);
+        }
+        public static Model GetInstance()
+        {
+            return _model;
         }
 
         public async Task Add()
         {
             await Task.Run(() =>
             {
-                for (var i = 0; i < 100; i++)
+                List.Clear();
+
+                Parallel.For(0, 100, i =>
                 {
-                    List.Add("hoge" + i.ToString());
-                }
+                    var source = new DataGridSource()
+                    {
+                        ColumnA = "A" + i.ToString(),
+                        ColumnB = "B" + i.ToString(),
+                        ColumnC = "C" + i.ToString(),
+                        ColumnD = "D" + i.ToString(),
+                        ColumnE = "E" + i.ToString(),
+                        ColumnF = "F" + i.ToString(),
+                        ColumnG = "G" + i.ToString(),
+                        ColumnH = "H" + i.ToString(),
+                        ColumnI = "I" + i.ToString(),
+                        ColumnJ = "J" + i.ToString()
+                    };
+
+                    lock (_lockObject)
+                    {
+                        List.Add(source);
+                    }
+                });
             });
         }
     }
--- MultiThreadCollection/ViewModel.cs
+++ MultiThreadCollection/ViewModel.cs
@@ -5,14 +5,15 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Data;
 
 namespace MultiThreadCollection
 {
     public class ViewModel
     {
-        private Model _model = new Model();
+        private Model _model = Model.GetInstance();
 
-        public ObservableCollection<string> List
+        public ObservableCollection<DataGridSource> List
         {
             get { return _model.List; }
             set { _model.List = value; }
